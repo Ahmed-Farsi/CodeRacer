35a3dd086af2248811319cfcf824379dd13e6c2c
Lin Wenhao <linwenhao5@gmail.com>
2021-05-11 12:06:08 +0200

    Uitwerking van de exercise

---
+++ berekening.php
@@ -0,0 +1,17 @@
+<?php
+$array = '<?php
+for ($i = 10; $i >= 1; $i--) {
+    echo $i . PHP_EOL;
+}
+?>';
+$delete = array(" ","　","\t","\n","\r");
+$tekens = str_replace($delete ,'',$array);
+$aantal_tekens = strlen($tekens);
+
+$tijd = 70;//seconds
+$berekening = $aantal_tekens/$tijd*60; 
+$TMP = round($berekening, 0);
+
+echo 'aantal tekens: ' . $aantal_tekens . ', TMP: ' . $TMP;
+
+?>
