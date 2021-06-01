e42196e9edb9af0ba6e7f60aeec684d111629057
Lin Wenhao <linwenhao5@gmail.com>
2021-06-01 10:12:26 +0200

    leaderboard en database

--- SQL/typegame.sql
+++ SQL/typegame.sql
@@ -34,14 +34,14 @@ var cars = ["BMW", "Volvo", "Saab", "Ford", "Fiat", "Audi"];
 var text = "";
 var i;
 for (i = 0; i < cars.length; i++) {
-  text += cars[i] + "<br>";
+    text += cars[i] + "<br>";
 }
 document.getElementById("demo").innerHTML = text;
 </script>', 'JS'),
 ('#include <stdio.h>
 int main() {
-   printf("Hello, World!");
-   return 0;
+    printf("Hello, World!");
+    return 0;
 }', 'C');
 
 Create table `leaderBoard`(
